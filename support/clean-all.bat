f489fd2819e700c4894ae51592747ab735650f1a
Joaquin Aenishanslin <joaquin.aenis.m@gmail.com>
2026-06-24 22:04:20 -0400

    Adicion de support y notificaciones

--- support/clean-all.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Limpiando proyecto de soporte...
-call mvn clean
-echo Carpetas temporales eliminadas.
-pause
--- support/src/main/java/support_service/support/repository/SupportTicketRepository.java
+++
@@ -1,9 +0,0 @@
-package support_service.support.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import support_service.support.model.SupportTicket;
-
-public interface SupportTicketRepository extends JpaRepository<SupportTicket, Long> {
-
-}
